1822b3ad8def1fe8735f46405786deb7f45542b8
Miminywang <132574373+Miminywang@users.noreply.github.com>
2023-12-02 11:31:50 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var hamburger = document.querySelector('.ham-icon');
-  hamburger.addEventListener('click', function(e) {
-    e.preventDefault();
-    var dropdown = document.querySelector('.dropdown');
-    dropdown.classList.toggle('active');
-  });
-});
